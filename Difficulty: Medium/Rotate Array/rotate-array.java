cd3877057be7c43449257d5fad4dc0a4e3f99ba9
Albab <116362742+albab786@users.noreply.github.com>
2026-05-27 14:03:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate Array/rotate-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    static void rotateArr(int arr[], int d) {
+        // code here
+      int n=arr.length;
+      d=d%n;
+      // Reverse the first d elements
+        reverse(arr, 0, d - 1);
+
+        // Reverse the remaining n-d elements
+        reverse(arr, d, n - 1);
+
+        // Reverse the entire array
+        reverse(arr, 0, n - 1);
+    }
+   static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
